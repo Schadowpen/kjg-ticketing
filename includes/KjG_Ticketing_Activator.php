c6ac2d9371d90f2366bac792ad76065700c01aa1
Philipp Horwat <philipp.horwat@peakwork.com>
2023-01-09 19:54:15 +0100

    WIP

--- includes/KjG_Ticketing_Activator.php
+++ includes/KjG_Ticketing_Activator.php
@@ -1,4 +1,5 @@
 <?php
+require_once get_home_path() . 'wp-admin/includes/upgrade.php';
 
 /**
  * Fired during plugin activation.
@@ -17,7 +18,6 @@ class KjG_Ticketing_Activator {
 	}
 
 	private static function create_database_tables() {
-		require_once get_home_path() . 'wp-admin/includes/upgrade.php';
 		
 		global $wpdb;
 		$charset_collate = $wpdb->get_charset_collate();
@@ -25,9 +25,24 @@ class KjG_Ticketing_Activator {
 		dbDelta("CREATE TABLE kjg_ticketing_events (
 			id int NOT NULL AUTO_INCREMENT,
 			name char(100) UNIQUE NOT NULL,
+			archived bit DEFAULT 0 NOT NULL,
 			ticket_price DECIMAL(6,2) DEFAULT 5 NOT NULL,
 			shipping_price DECIMAL(6,2) DEFAULT 2.5 NOT NULL,
+			seating_plan_width FLOAT NOT NULL,
+			seating_plan_length FLOAT NOT NULL,
+			seat_width FLOAT NOT NULL,
+			seat_length FLOAT NOT NULL,
+			seating_plan_length_unit char(32) NOT NULL,
 			PRIMARY KEY  (id)
+			) $charset_collate;");
+		
+		dbDelta("CREATE TABLE kjg_ticketing_ticket_config (
+			id int NOT NULL,
+			template blob NOT NULL,
+			seating_plan_seat_numbers_visible bit DEFAULT 0 NOT NULL,
+			seating_plan_connect_arrows bit DEFAULT 1 NOT NULL,
+			PRIMARY KEY  (id),
+			FOREIGN KEY  (id) REFERENCES kjg_ticketing_events(id)
 			) $charset_collate;");
 	}
 }
