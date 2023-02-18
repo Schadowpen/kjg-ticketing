86c011bc9dd8dee87fdb346ac99a176eb7b4a9ab
Philipp Horwat <philipp.horwat@t-online.de>
2023-02-18 17:49:21 +0100

    Delete tables on uninstall

--- includes/KjG_Ticketing_Uninstaller.php
+++ includes/KjG_Ticketing_Uninstaller.php
@@ -13,7 +13,44 @@ class KjG_Ticketing_Uninstaller {
 	 * Long Description.
 	 */
 	public static function uninstall() {
+		self::delete_database_tables();
+	}
 
+	private static function delete_database_tables() {
+		self::delete_database_tables_active();
+		self::delete_database_tables_template();
+
+		delete_option( 'kjg_ticketing_db_version' );
+	}
+
+	private static function delete_database_tables_active() {
+		global $wpdb;
+
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_seat_state" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_shows" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_process_additional_entries" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_process_additional_fields" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_processes" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_seats" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_entrances" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_seating_plan_areas" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_ticket_image_config" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_ticket_text_config" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_events" );
+	}
+
+	private static function delete_database_tables_template() {
+		global $wpdb;
+
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_shows" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_process_additional_fields" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_seat_groups" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_seats" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_entrances" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_seating_plan_areas" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_ticket_image_config" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_ticket_text_config" );
+		$wpdb->query( "DROP TABLE IF EXISTS kjg_ticketing_template_events" );
 	}
 
 }
