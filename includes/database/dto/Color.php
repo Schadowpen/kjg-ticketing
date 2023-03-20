124a23ecfddcb3bedb18829c602d35515019d60a
Philipp Horwat <philipp.horwat@t-online.de>
2023-03-20 21:37:32 +0100

    Fix 8-Bit Color value being returned in range -128 to 127

--- includes/database/dto/Color.php
+++ includes/database/dto/Color.php
@@ -23,14 +23,23 @@ class Color {
     public int $blue;
 
     /**
-     * @param int $red Value of red in range 0 to 255
-     * @param int $green Value of green in range 0 to 255
-     * @param int $blue Value of blue in range 0 to 255
+     * Constructs a Color with RGB values in range 0 to 255
+     * Values down to -128 are wrapped around to support signed 8-bit integers.
+     *
+     * It is not checked if a value is outside this range
      */
     public function __construct( int $red, int $green, int $blue ) {
-        $this->red = $red;
-        $this->green = $green;
-        $this->blue = $blue;
+        $this->red   = $this->from_maybe_negative( $red );
+        $this->green = $this->from_maybe_negative( $green );
+        $this->blue  = $this->from_maybe_negative( $blue );
+    }
+
+    private function from_maybe_negative( int $color_value ): int {
+        if ( $color_value < 0 && $color_value >= - 128 ) {
+            $color_value += 256;
+        }
+
+        return $color_value;
     }
 
     public function to_PDF_color(): ColorRGB {
